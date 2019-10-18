9aadce5ec5f1dfc220233a32c8ed2ce954da0ed9
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-18 20:35:30 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species # code goes here
+  
+  @@all = []
+  @cats = []
+  @dogs = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
 end
