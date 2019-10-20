f81db727470c150f425cdd37b6a338e91a55b4ea
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-20 13:46:23 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    puts "I am a #{species}."
+  end
+
+
 end
