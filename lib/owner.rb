c4457e64d5d7c568efea26055f0e868bd5bb97cd
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-10-20 05:47:42 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,19 +1,17 @@
 class Owner
-  attr_reader :name, :species, :pets
+  # code goes here
+  attr_reader :name, :species
+
   @@all = []
+
   def initialize(name)
     @name = name
     @species = "human"
-    @pets = {}
-    self.save
-  end
-
-  def save
     @@all << self
   end
 
   def say_species
-    "I am a #{@species}."
+    "I am a #{self.species}."
   end
 
   def self.all
@@ -21,77 +19,44 @@ class Owner
   end
 
   def self.count
-    @@all.length
+    self.all.count
   end
 
   def self.reset_all
-    @@all.clear
-  end
-
-  def buy_pet(pet, pet_category)
-    if self.pets.has_key?(pet_category) 
-      pets[pet_category] << pet unless pets[pet_category].include?(pet)
-    else
-      pets[pet_category] = [pet]
-    end
-    pet.owner = self
-  end
-
-  def buy_helper(pet_input, pet_category, pet_category_class)
-    if pet_input.is_a?(pet_category_class)
-      buy_pet(pet_input, pet_category) 
-    elsif pet_input.is_a?(String) 
-      pet_proper = pet_category_class.all.find{|pet_object| pet_object.name == pet_input} 
-      if pet_proper 
-        buy_pet(pet_proper, pet_category) 
-      else
-        buy_pet(pet_category_class.new(pet_input, self), pet_category) 
-      end
-    else
-      puts "Not a #{pet_category.to_s.chop}."
-    end
-  end
-
-  def buy_cat(cat)
-    buy_helper(cat, :cats, Cat)
-  end
-
-  def buy_dog(dog)
-    buy_helper(dog, :dogs, Dog)
+    self.all.clear
   end
 
   def cats
-    if pets[:cats]
-      pets[:cats]
-    else
-      []
-    end
+    Cat.all.select{|cat| cat.owner == self}
   end
 
   def dogs
-    if pets[:dogs]
-      pets[:dogs]
-    else
-      []
-    end
+    Dog.all.select{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
   end
 
   def walk_dogs
-    self.dogs.each{|dog| dog.mood = "happy"}
+    self.dogs.each { |dog| dog.mood = "happy" }
   end
 
   def feed_cats
-    self.cats.each{|cat| cat.mood = "happy"}
+    self.cats.each { |cat| cat.mood = "happy" }
   end
 
   def sell_pets
-    self.pets.each do |category, array|
-      array.each do |pet|
-        pet.mood = "nervous"
-        pet.owner = nil
-      end
+    pets = self.dogs + self.cats
+
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
     end
-    self.pets.clear
   end
 
   def list_pets
