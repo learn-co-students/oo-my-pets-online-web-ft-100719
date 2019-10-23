4958e1595c5ef13759414043b085371c91fb20f8
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-23 15:57:49 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,81 @@
+
+#   Class methods
+#     .all returns all instances of Owner that have been created (FAILED - 6)
+#     .count returns the number of owners that have been created (FAILED - 7)
+#     .reset_all can reset the owners that have been created (FAILED - 8)
+
+require 'pry'
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+
+  @@all=[]
+
+  def initialize(name)
+    @name=name
+    @species= "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+
+  def cats
+    Cat.all.select{|cat| cat.owner==self}
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner==self}
+  end
+
+  def buy_cat(cat)
+    cat=Cat.new(cat, self)
+    cat.owner=self
+  end
+
+  def buy_dog(dog)
+    dog=Dog.new(dog, self)
+    dog.owner=self
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+    # @pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.cats.each do |cat|
+      cat.mood="nervous"
+      cat.owner=nil
+    end
+    self.dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner= nil
+    end
+    end
+
+  def list_pets
+    return "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+
+
 end
